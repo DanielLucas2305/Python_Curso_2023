8b99992e2a4c9b7aee27910b1ccd76d551801732
Daniel Lucas <danieldodesign2005@gmail.com>
2023-03-10 14:47:32 -0300

    curso

--- Pagamentos.py
+++ Pagamentos.py
@@ -1,6 +1,6 @@
 print("{:*^52}".format(" DANIEL's SHOP "))
 prodprice = float(input("Insira o valor do produto: "))
-print("Escolha uma forma de pagamento\n<1> À vista\n<2> À vista no cartão\n<3> 2x no cartão\n<4> 3x ou mais")
+print("Escolha uma forma de pagamento\n<1> À vista(10% de desconto)\n<2> À vista no cartão(5% de desconto)\n<3> 2x no cartão\n<4> 3x ou mais(20% de juros)")
 option = int(input(":"))
 
 
@@ -11,10 +11,12 @@ elif option == 2:
     vista = (prodprice * 95) / 100 ##a variavel pode ser o mesmo nome da de cima###
     print("Você pagará R$%.2f reais" %vista)
 elif option == 3:
-    print("Você pagará R$%.2f reais" %prodprice)
+    parce = prodprice / 2
+    print("Você pagará 2 parcelas de R$%.2f reais" %parce)
 elif option == 4:
-    parce = ((prodprice * 20) / 100) + prodprice
-    print("Você pagará R$%.2f reais" %parce)
+    vezes = int(input("Quantas vezes deseja parcelar ? "))
+    parce = (prodprice + (prodprice * 20 / 100)) / vezes
+    print("Você pagará %i parcelas de R$%.2f reais" %(vezes, parce))
 else:
     print("Opção inválida !!! Tente novamente.")
 
