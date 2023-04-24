8a116cad2bf0bb5bb5accb64c139f3c42a50379e
Daniel Lucas <danieldodesign2005@gmail.com>
2023-04-24 12:19:13 -0300

    more one

--- LerMouF.py
+++ LerMouF.py
@@ -1,6 +1,7 @@
 r = "M" or "F"
-while r == "M" or r == "F":
+while r != "M" or r != "F":
     r = str(input("Digite o sexo [M/F]: ")).upper()
-    if r != "M":
-        if r != "F":
-            print("Digite M ou F !!!")
+    if r != "M" or r != "F":
+        print("Digite M ou F !!!")
+        # if r != "F":
+        #     print("Digite M ou F !!!")    #pode ser feito dessa maneira, porem com mais linhas
